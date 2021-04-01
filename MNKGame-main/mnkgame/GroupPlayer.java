e11ce58b70ebb9935a92165376f8cd187cab7a74
Campanelli Emanuele <76498176+CampanelliEmanuele@users.noreply.github.com>
2021-04-01 22:21:06 +0200

    Add files via upload

---
+++ MNKGame-main/mnkgame/GroupPlayer.java
@@ -0,0 +1,49 @@
+package mnkgame;
+
+import java.util.Random;
+
+public class GroupPlayer implements MNKPlayer{
+	
+	private MNKBoard B;
+	
+	private MNKGameState myWin;
+
+	private MNKGameState yourWin;
+
+	private Random rand;
+
+	public GroupPlayer(){}
+
+	public void initPlayer(int M, int N, int K, boolean first){
+	
+		rand = new Random(System.currentTimeMillis());
+		B = new MNKBoard(M,N,K);
+
+		//myWin: vittoria di questo giocatore
+		myWin = first ? MNKGameState.WINP1 : MNKGameState.WINP2;
+		
+		//Vittoria dell'avversario
+		yourWin = first ? MNKGameState.WINP2 : MNKGameState.WINP1;	
+	}
+
+	/*
+	 * Deve ritornare la cella in cui mettere il segno
+	 */
+	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC){
+		System.out.println(FC[0].hashCode());
+
+		//se rimane una singola mossa da fare
+		if(FC.length == 1)
+			return FC[0];
+
+		return FC[0];	
+	}
+
+	public String playerName(){
+		return "";
+	}
+
+	public static void main (String[] args) {
+		System.out.println("lol");
+	}
+}
---
+++ MNKGame-main/mnkgame/Main.java
@@ -0,0 +1,28 @@
+package mnkgame;
+
+public class Main {
+
+    // Per memorizzare i sottoalberi verrà utilizzata una struttura dati dinamica omogena lineare
+
+    public static void creaSottoAlbero (TreeNode in_alberoPadre, MNKBoard in_boardPadre, MNKCell[] FC) {
+        for (int k = 0; k < FC.length; k++) {               // Crea un nuovo sotto-albero per ogni cella libera (rappresentazione di ogni possible mossa)
+        MNKBoard tmpBoard = in_boardPadre;                // Creazione della nuova board
+        tmpBoard.markCell (FC[k].i, FC[k].j);             // Marcamento della board appena creata
+        TreeNode nuovoSottoAlbero = new TreeNode (tmpBoard, in_alberoPadre);    // Creazione del sottoalbero con la nuova board
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MNKBoard bp = new MNKBoard(3, 3, 3);
+        TreeNode padre = new TreeNode(bp);
+
+        MNKBoard bf1 = new MNKBoard(3, 3, 3);
+        TreeNode figlio1 = new TreeNode(bf1, padre);
+
+        MNKBoard bf2 = new MNKBoard(3, 3, 3);
+        TreeNode figlio2 = new TreeNode(bf2, padre);
+
+        // System.out.println("loool");
+    }
+}
